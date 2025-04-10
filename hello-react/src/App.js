6bdd6aa1cd4fc7cbdd18fa312dba100061b1bcf5
hyChunjae2023 <hyChunjae2023@gmail.com>
2025-04-10 11:25:40 +0900

    ref 컴포넌트에 설정하여 컴포넌트 내부 DOM 요소 직접 제어

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import ValidationSample from './Components/ValidationSample';
+import { Component, createRef } from 'react';
+import ScrollBox from './Components/ScrollBox';
 
+class App extends Component {
+  scrollBox = createRef();
+  render() {
+    return (
+      <div>
+        {/** 컴포넌트에 ref 를 설정하여 컴포넌트 내부의 DOM 요소 직접 제어 */}
+        <ScrollBox ref={(ref) => this.scrollBox = ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 아래로</button>
+        <button onClick={() => this.scrollBox.scrollToTop()}>맨 위로</button>
+      </div>
+    )
+  }  
+}
+
+/*
 const App = () => {
   return (
     <div>
-      <ValidationSample />
     </div>
   )
 }
-
+*/
 export default App;
