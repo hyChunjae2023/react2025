e5f897e561bdf6e967bc88242f84759202e8faec
hyChunjae2023 <hyChunjae2023@gmail.com>
2025-04-10 10:07:21 +0900

    event_useState_object

--- hello-react/src/Components/EventPracticsFunc.js
+++ hello-react/src/Components/EventPracticsFunc.js
@@ -1,14 +1,38 @@
 import { useState } from 'react';
 
 const EventPracticeFunc = () => {
-    const [ userName, setUserName ] = useState("");
-    const [ message, setMessage ] = useState("");
-    const onChangeUserName = e => setUserName(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
-    const onClick = () => {
-        setUserName("");
-        setMessage("");
+    /** state 에 문자열 데이터 */
+    //const [ userName, setUserName ] = useState("");
+    //const [ message, setMessage ] = useState("");
+    //const onChangeUserName = e => setUserName(e.target.value);
+    //const onChangeMessage = e => setMessage(e.target.value);
+    //const onClick = () => {
+    //    setUserName("");
+    //    setMessage("");
+    //};
+
+    /** state 에 객체 데이터 :
+    //   ㄴ e.target.name 을 활용하려면 state 객체 데이터의 속성값을 e.target.name 을 사용 */
+    const [ form, setForm ] = useState({
+        userName: "",
+        message: ""
+    });
+    const { userName, message } = form;//getter
+    const onChange = e => {
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value
+        };
+        setForm(nextForm);
     };
+    const onClick = () => {
+        setForm({
+            userName: "",
+            message: ""
+        });
+    };
+
+
     const onKeyUp = e => (e.key === "Enter") ? onClick() : null;
 
     return(
@@ -19,7 +43,8 @@ const EventPracticeFunc = () => {
                 name="userName"
                 placeholder='사용자명'
                 value={userName}
-                onChange={onChangeUserName}
+                //onChange={onChangeUserName}
+                onChange={onChange}
                 onKeyUp={onKeyUp}
             />
             <input 
@@ -27,7 +52,8 @@ const EventPracticeFunc = () => {
                 name="message"
                 placeholder="내용"
                 value={message}
-                onChange={onChangeMessage}
+                //onChange={onChangeMessage}
+                onChange={onChange}
                 onKeyUp={onKeyUp}
             />
             <button 
